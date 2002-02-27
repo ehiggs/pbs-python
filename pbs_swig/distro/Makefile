e48aaa779f26b4e01adcd65782f4fe522b62d529
sscpbas <sscpbas@d2e01e3e-261c-0410-ba37-90eb7937b9b9>
2002-02-27 13:55:46 +0000

    Preparing for new distro, included resmom functions
    git-svn-id: https://oss.trac.surfsara.nl/pbs_python/svn/trunk@11 d2e01e3e-261c-0410-ba37-90eb7937b9b9

--- pbs_swig/distro/Makefile
+++ pbs_swig/distro/Makefile
@@ -1,11 +1,12 @@
 # 
-# $Id: Makefile,v 1.3 2001/12/07 19:14:23 sscpbas Exp $
+# $Id: Makefile,v 1.4 2002/02/27 13:55:43 sscpbas Exp $
 #
-LIBS    =  -L/usr/local/lib -lpbs
-INCLUDE =  -I/usr/include/python1.5
-#INCLUDE =  -I/usr/local/include/python2.1
+LIBS    =  -L/usr/local/lib -lnet -lpbs
+#INCLUDE =  -I/usr/include/python1.5
+INCLUDE =  -I/usr/local/include/python2.1
 
 pbs::
+	/usr/bin/swig -python -shadow pbs.i
 	gcc -c pbs_wrap.c ${INCLUDE}
 	ld -G pbs_wrap.o ${LIBS} -o pbscmodule.so
 
---
+++ pbs_swig/distro/resmom.py
@@ -0,0 +1,108 @@
+#  PBS python interface
+#  Author: Bas van der Vlies <basv@sara.nl>
+#  Date  : 27 Feb 2002
+#  Desc. : This is python wrapper class for getting the resource
+#          mom values.
+#
+# CVS info
+# $Id: resmom.py,v 1.1 2002/02/27 13:55:45 sscpbas Exp $
+# $Date: 2002/02/27 13:55:45 $
+# $Revision: 1.1 $
+#
+import string
+import types
+
+# Default linux resources to get from the mom
+#
+default_linux_res = [   
+	"availmem",	# available memory size in KB
+	"ideal_load",	# static ideal_load value
+	"loadave",      # the current load average
+	"max_load",	# static max_load value
+	"ncpus",        # number of cpus
+	"physmem",      # physical memory size in KB
+	"resi",		# resident memory size for a pid or session in KB
+	"totmem",	# total memory size in KB
+	"walltime",	# wall clock time for a pid
+]
+
+# Default irix6 resources to get from the mom
+#
+default_irix6_res = [   
+	"availmem",	# available memory size in KB
+	"loadave",      # the current load average
+	"ncpus",        # number of cpus
+	"physmem",      # physical memory size in KB
+	"resi",		# resident memory size for a pid or session in KB
+	"walltime",	# wall clock time for a pid
+	"quota",	# quota information (sizes in KB)
+]
+
+default_mom_res = [   
+	"arch",		# the architecture of the machine
+	"uname",	# the architecture of the machine
+        "cput",		# cpu time for a pid or session
+	"idletime",	# seconds of idle time
+	"mem",		# memory size for a pid or session in KB
+	"sessions",	# list of sessions in the system
+	"pids",         # list of pids in a session
+	"nsessions",	# number of sessions in the system
+	"nusers",	# number of users in the system
+	"size",		# size of a file or filesystem
+]
+
+def check_resp(dict, str):
+
+    key, val = string.split(str, '=')
+    key = string.strip(key)
+    val = string.strip(val)
+
+    # Did we got a valid response
+    #
+    if not val[0] == '?':
+      dict[key] = val
+
+def use_default_keywords(id, d):
+  """
+  Get the default values from the mom daemon
+  """
+  for res in default_mom_res:
+    addreq(id, res)
+    resp = getreq(id)
+    check_resp(d, resp)
+
+  if d['arch' ] == 'linux':
+    for res in default_linux_res:
+      addreq(id, res)
+      resp = getreq(id)
+      check_resp(d, resp)
+
+def use_user_keywords(id, d, l):
+  for res in l:
+    if type(res) is types.StringType:
+      addreq(id, res)
+      resp = getreq(id)
+      check_resp(d, resp)
+    else:
+      raise TypeError, 'Expected a string got %s :%s' %(type(res), res) 
+
+def get_mom_values(id, list = None):
+  """
+  This function will query the mom with a default resmon keywords
+  and 'arch' depended keywords. Supported archs are:
+    linux
+    irix6
+  User can also supply their own list of keywords as second parameter.
+  arguments:
+    id   : connection number with mom daemon on a node
+    list : optional parameter. If supplied then use this. A list
+           of mom keywords.
+  """
+
+  d = {}
+  if not list:
+    use_default_keywords(id, d)
+  else:
+    use_user_keywords(id, d , list)
+     
+  return d
