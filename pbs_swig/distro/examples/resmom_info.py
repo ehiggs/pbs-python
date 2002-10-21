5fb346a675dad87b394034b63e496ae0b7f8627d
sscpbas <sscpbas@d2e01e3e-261c-0410-ba37-90eb7937b9b9>
2002-10-21 14:05:34 +0000

    Fixed a couple of bugs see CHANGES
    git-svn-id: https://oss.trac.surfsara.nl/pbs_python/svn/trunk@28 d2e01e3e-261c-0410-ba37-90eb7937b9b9

--- pbs_swig/distro/examples/resmom_info.py
+++ pbs_swig/distro/examples/resmom_info.py
@@ -4,7 +4,7 @@
 # Date  : 27 Feb 2002
 # Desc. : This will query the pbs_mom daemon about its resources
 #
-# $Id: resmom_info.py,v 1.1 2002/02/27 14:35:51 sscpbas Exp $
+# $Id: resmom_info.py,v 1.2 2002/10/21 14:05:33 sscpbas Exp $
 #
 import pbs
 import sys
@@ -23,11 +23,13 @@ def main():
     for attr in attrs:
       print '\t%s = %s' %(attr.name, attr.value)
 
-    mom_port = socket.getservbyname('pbs_resmom', 'tcp')
+    mom_port = socket.getservbyname('pbs_resmon', 'tcp')
     if mom_port:
       mom_id = pbs.openrm(node.name, mom_port)
     else:
       mom_id = pbs.openrm(node.name, pbs.PBS_MANAGER_SERVICE_PORT)
+
+    print "mom_id = ", mom_id
 
     mom_keys = pbs.get_mom_values(mom_id)
     for key in mom_keys.keys():
@@ -35,7 +37,7 @@ def main():
 
     print '\nTesting list with user supplied keywords'
 
-    l = [ 'ncpus', 'loadave' ]
+    l = [ 'bas', 'ncpus', 'loadave' ]
     mom_keys = pbs.get_mom_values(mom_id, l)
     for key in mom_keys.keys():
       print '\t%s = %s' %(key, mom_keys[key])
