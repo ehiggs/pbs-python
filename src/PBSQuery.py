99d6b6c3293713a459e3a723aaa6c6d67a4b0e36
bas <bas@d2e01e3e-261c-0410-ba37-90eb7937b9b9>
2006-10-18 09:19:04 +0000

    src/PBSQuery.py: 	- rewrite/indent 	- Make use of UserDict, removed a lot of code 	- Still a lot of debug code ;-)
    git-svn-id: https://oss.trac.surfsara.nl/pbs_python/svn/trunk@139 d2e01e3e-261c-0410-ba37-90eb7937b9b9

--- src/PBSQuery.py
+++ src/PBSQuery.py
@@ -59,266 +59,252 @@ you are interested in, eg: only show state of nodes
 	nodes = p.getnodes(l)
 """
 import pbs
+import UserDict
+import sys
 
 class PBSError(Exception):
-        def __init__(self, msg=''):
-                self.msg = msg
-                Exception.__init__(self, msg)
-        def __repr__(self):
-                return self.msg
-        __str__ = __repr__
+	def __init__(self, msg=''):
+		self.msg = msg
+		Exception.__init__(self, msg)
+		
+	def __repr__(self):
+		return self.msg
+
+	__str__ = __repr__
 
 
 class PBSQuery:
-    def __init__(self, server=None):
-        if not server:
-            self.server = pbs.pbs_default()
-	else:
-	    self.server = server
-
-
-    def _connect(self):
-       """Connect to the PBS/Torque server"""
-       self.con = pbs.pbs_connect(self.server)
-       if self.con < 0:
-          str = "Could not make an connection with %s\n" %(self.server)
-          raise PBSError(str)
-
-    def _disconnect(self):
-       """Close the PBS/Torque connection"""
-       pbs.pbs_disconnect(self.con)
-       self.attribs = 'NULL'
-
-    def _list_2_attrib(self, list):
-        """Convert an python list to an attrib list suitable for pbs"""
-        self.attribs = pbs.new_attrl( len(list) )
-        i = 0 
-        for attrib in list:
-	    self.attribs[i].name = attrib
-	    i = i + 1
-
-    def _pbsstr_2_list(self, str, delimiter):
-        """Convert an string to an python list and use delimiter as spit char"""
-	l = sting.splitfields(str, delimiter)
-	if len(l) > 1:
-	   return l
-
-    def _list_2_dict(self, l, class_func):
-        """Convert an pbsstat function list to an class dictionary"""
-        self.d = {}
-        for item in l:
-	    new = class_func()
-	    for a in item.attribs:
-	        new.name = item.name 
-	        if a.resource:
-                   new.attribs[a.name + '.' + a.resource] = a.value
+	def __init__(self, server=None):
+		if not server:
+			self.server = pbs.pbs_default()
 		else:
-	           new.attribs[a.name] = a.value
-                self.d[item.name] = new
-        self._free(l)
+			self.server = server
+
+
+	def _connect(self):
+		"""Connect to the PBS/Torque server"""
+		self.con = pbs.pbs_connect(self.server)
+		if self.con < 0:
+			str = "Could not make an connection with %s\n" %(self.server)
+			raise PBSError(str)
+
+	def _disconnect(self):
+		"""Close the PBS/Torque connection"""
+		pbs.pbs_disconnect(self.con)
+		self.attribs = 'NULL'
+
+	def _list_2_attrib(self, list):
+		"""Convert an python list to an attrib list suitable for pbs"""
+		self.attribs = pbs.new_attrl( len(list) )
+		i = 0 
+		for attrib in list:
+			self.attribs[i].name = attrib
+			i = i + 1
+
+	def _pbsstr_2_list(self, str, delimiter):
+		"""Convert an string to an python list and use delimiter as spit char"""
+		l = sting.splitfields(str, delimiter)
+		if len(l) > 1:
+			return l
+
+	def _list_2_dict(self, l, class_func):
+		"""Convert an pbsstat function list to an class dictionary"""
+		self.d = {}
+		for item in l:
+			new = class_func()
+			self.d[item.name] = new 
+			
+			for a in item.attribs:
+				new.name = item.name 
+				if a.resource:
+					#new.attribs[a.name + '.' + a.resource] = a.value
+					new[a.name + '.' + a.resource] = a.value
+				else:
+					#new.attribs[a.name] = a.value
+					new[a.name] = a.value
+
+		#print new
+		print 'Basje'
+		print self.d['q_express'].has_key('resources_max.walltime')
+		#print 'items ', new.items() 
+		q_attr =  self.d['q_express']
+		print 'q_attr: ', type(q_attr)
+		print 'q_attr class: ', q_attr.__class__
+		print 'has_key', q_attr.has_key('resources_max.walltime')
+		self._free(l)
 	        
-    def _free(self, memory):
-        pbs.pbs_statfree(memory)
+	def _free(self, memory):
+		pbs.pbs_statfree(memory)
 
-    def _statserver(self, attrib_list=None):
-        """Get the server config from the pbs server"""
-        if attrib_list:
-	   self._list_2_attrib(attrib_list)
-        else:
-	   self.attribs = 'NULL'
+	def _statserver(self, attrib_list=None):
+		"""Get the server config from the pbs server"""
+		if attrib_list:
+			self._list_2_attrib(attrib_list)
+		else:
+			self.attribs = 'NULL' 
+			
+		self._connect()
+		serverinfo = pbs.pbs_statserver(self.con, self.attribs, 'NULL')
+		self._disconnect() 
+		
+		self.serverinfo = {}
+		self._list_2_dict(serverinfo, server)
 
-        self._connect()
-        serverinfo = pbs.pbs_statserver(self.con, self.attribs, 'NULL')
-        self._disconnect()
+	def get_serverinfo(self, attrib_list=None):
+		self._statserver(attrib_list)
+		return self.d
 
-	self.serverinfo = {}
-	self._list_2_dict(serverinfo, server)
+	def _statqueue(self, queue_name='', attrib_list=None):
+		"""Get the queue config from the pbs server"""
+		if attrib_list:
+			self._list_2_attrib(attrib_list)
+		else:
+			self.attribs = 'NULL' 
+			
+		self._connect()
+		queues = pbs.pbs_statque(self.con, queue_name, self.attribs, 'NULL')
+		self._disconnect() 
+		
+		self._list_2_dict(queues, queue)
 
-    def get_serverinfo(self, attrib_list=None):
-        self._statserver(attrib_list)
-	return self.d
-
-    def _statqueue(self, queue_name='', attrib_list=None):
-        """Get the queue config from the pbs server"""
-        if attrib_list:
-	   self._list_2_attrib(attrib_list)
-        else:
-	   self.attribs = 'NULL'
-
-        self._connect()
-        queues = pbs.pbs_statque(self.con, queue_name, self.attribs, 'NULL')
-        self._disconnect()
-
-	self.queues = {}
-	self._list_2_dict(queues, queue)
-
-    def getqueue(self, name, attrib_list=None):
-        self._statqueue(name, attrib_list)
-	return self.d
+	def getqueue(self, name, attrib_list=None):
+		self._statqueue(name, attrib_list)
+		print 'getqueue ', self.d['q_express'].has_key('resources_max.walltime')
+		q_attrs = self.d['q_express']
+		print 'getqueue has_key', q_attrs.has_key('resources_max.walltime') 
+		return self.d
         
-    def getqueues(self, attrib_list=None):
-        self._statqueue('', attrib_list)
-	return self.d
+	def getqueues(self, attrib_list=None):
+		self._statqueue('', attrib_list)
+		return self.d
 
-    def _statnode(self, node_name='', attrib_list=None):
-        """Get the node config from the pbs server"""
-        if attrib_list:
-	   self._list_2_attrib(attrib_list)
-        else:
-	   self.attribs = 'NULL'
+	def _statnode(self, node_name='', attrib_list=None):
+		"""Get the node config from the pbs server"""
+		if attrib_list:
+			self._list_2_attrib(attrib_list)
+		else:
+			self.attribs = 'NULL' 
+			
+		self._connect()
+		nodes = pbs.pbs_statnode(self.con, node_name, self.attribs, 'NULL')
+		self._disconnect() 
+		
+		self.nodes = {}
+		self._list_2_dict(nodes, node)
 
-        self._connect()
-        nodes = pbs.pbs_statnode(self.con, node_name, self.attribs, 'NULL')
-        self._disconnect()
-
-        self.nodes = {}
-	self._list_2_dict(nodes, node)
-
-    def getnode(self, name, attrib_list=None):
-        self._statnode(name, attrib_list)
-	return self.d
+	def getnode(self, name, attrib_list=None):
+		self._statnode(name, attrib_list)
+		return self.d
         
-    def getnodes(self, attrib_list=None):
-        self._statnode('', attrib_list)
-	return self.d
+	def getnodes(self, attrib_list=None):
+		self._statnode('', attrib_list)
+		return self.d
 
-    def _statjob(self, job_name='', attrib_list=None):
-        """Get the job config from the pbs server"""
-        if attrib_list:
-	   self._list_2_attrib(attrib_list)
-        else:
-	   self.attribs = 'NULL'
-	   
-        self._connect()
-        jobs = pbs.pbs_statjob(self.con, job_name, self.attribs, 'NULL')
-        self._disconnect()
+	def _statjob(self, job_name='', attrib_list=None):
+		"""Get the job config from the pbs server"""
+		if attrib_list:
+			self._list_2_attrib(attrib_list)
+		else:
+			self.attribs = 'NULL' 
+			
+		self._connect()
+		jobs = pbs.pbs_statjob(self.con, job_name, self.attribs, 'NULL')
+		self._disconnect() 
+		
+		self.jobs = {}
+		self._list_2_dict(jobs, job)
 
-        self.jobs = {}
-	self._list_2_dict(jobs, job)
-
-    def getjob(self, name, attrib_list=None):
-        self._statjob(name, attrib_list)
-	return self.d
+	def getjob(self, name, attrib_list=None):
+		self._statjob(name, attrib_list)
+		return self.d
         
-    def getjobs(self, attrib_list=None):
-        self._statjob('', attrib_list)
-	return self.d
+	def getjobs(self, attrib_list=None):
+		self._statjob('', attrib_list)
+		return self.d
 
-class _PBSobject:
-    TRUE  = 1
-    FALSE = 0
+class _PBSobject(UserDict.UserDict):
+	TRUE  = 1
+	FALSE = 0
 
-    def __init__(self):
-        self.attribs = {}
-	self.name = None
+	def __init__(self):
+		UserDict.UserDict.__init__(self)
+		self.name = None
 
-    def __len__(self):
-    	return(len(self.attribs.keys()))
+	def get_value(self, key):
+		if self.has_key(key):
+			return self[key]
+		else:
+			return None
 
-    def __str__(self):
-        data = self.name + '\n'
-	for name,value in self.attribs.items():
-                data = data + '\t%s = %s\n' %(name, value)
-	return(data)
-
-    __repr__ = __str__
-
-    def __getitem__(self,key):
-        if key == 'name':
-            return self.name
-        
-    	return(self.attribs[key])
-
-    def get_value(self, key):
-        if self.attribs.has_key(key):
-	   return self.attribs[key]
-        else:
-	   return None
-
-    def has_key(self, key):
-        if self.attribs.has_key(key):
-	   return self.TRUE
-        else:
-	   return self.FAlSE
-
-    def keys(self):
-        return self.attribs.keys()
-
-    def values(self):
-        return self.attribs.values()
-
-    def items(self):
-        return self.attribs.items()
-    
 class job(_PBSobject):
-    """PBS job class"""
-
-    def is_running(self):
-    	if self.get_value('job_state') == 'Q':
-	   return self.FALSE
-	else:
-	   return self.TRUE 
+	"""PBS job class""" 
+	def is_running(self):
+		if self.get_value('job_state') == 'Q':
+			return self.FALSE
+		else:
+			return self.TRUE 
 
 class node(_PBSobject):
-    """PBS node class"""
+	"""PBS node class"""
+	
+	def is_free(self):
+		"""Check if node is free"""
+		if self.get_value('state') == 'free':
+			return self.TRUE
+		else: 
+			return self.FALSE 
 
-    def is_free(self):
-        """Check if node is free"""
-        if self.get_value('state') == 'free':
-	      return self.TRUE
-	else:
-	      return self.FALSE 
-
-    def has_job(self):
-        """Does the node run a job"""
-        if self.get_value('jobs'):
-	      return self.TRUE
-	else:
-	      return self.FALSE
+	def has_job(self):
+		"""Does the node run a job"""
+		if self.get_value('jobs'):
+			return self.TRUE
+		else:
+			return self.FALSE
 
 class queue(_PBSobject):
-    """PBS queue class"""
+	"""PBS queue class"""
+	def __init__(self):
+		UserDict.UserDict.__init__(self)
+		self.name = None
 
-    def is_enabled(self):
-    	if self.get_value('enabled') == 'True':
-	   return self.TRUE 
-	else:
-	   return self.FALSE
+	def is_enabled(self):
+		if self.get_value('enabled') == 'True':
+			return self.TRUE 
+		else:
+			return self.FALSE
 
-    def is_execution(self):
-    	if self.get_value('queue_type') == 'Execution':
-	   return self.TRUE 
-	else:
-	   return self.FALSE
-
+	def is_execution(self):
+		if self.get_value('queue_type') == 'Execution':
+			return self.TRUE 
+		else:
+			return self.FALSE
 
 class server(_PBSobject):
-    """PBS server class"""
+	"""PBS server class"""
 
 def main():
-    p = PBSQuery()
-
-    serverinfo = p.get_serverinfo()
-    for name, server in serverinfo.items():
-	print server
+	p = PBSQuery() 
+	serverinfo = p.get_serverinfo()
+	for name, server in serverinfo.items():
+		print server
 	for key in server.keys():
 		print key, ' = ', server[key]
 
-    queues = p.getqueues()
-    for queue in queues.values():
-	if queue.is_execution():
-	   print queue
+	queues = p.getqueues()
+	for queue in queues.values():
+		if queue.is_execution():
+			print queue
 
-    jobs = p.getjobs()
-    for name,job in jobs.items():
-	if job.is_running():
-	   print job
+	jobs = p.getjobs()
+	for name,job in jobs.items():
+		if job.is_running():
+			print job
 
-    l = ['state']
-    nodes = p.getnodes(l)
-    for name,node in nodes.items():
-        if node.is_free(): 
-           print node
+	l = ['state']
+	nodes = p.getnodes(l)
+	for name,node in nodes.items():
+		if node.is_free(): 
+			print node
 
 if __name__ == "__main__":
-    main()
+	main()
