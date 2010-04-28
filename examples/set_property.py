3285f4dcfb84dc6a04f7418907c702dac30b031c
bas <bas@d2e01e3e-261c-0410-ba37-90eb7937b9b9>
2010-04-28 08:47:27 +0000

    examples/set_property.py: 	- Now accept a hostname as argument
    git-svn-id: https://oss.trac.surfsara.nl/pbs_python/svn/trunk@235 d2e01e3e-261c-0410-ba37-90eb7937b9b9

--- examples/set_property.py
+++ examples/set_property.py
@@ -18,15 +18,21 @@ def main():
     print 'No default server'
     sys.exit(1)
 
+  if len(sys.argv) < 2:
+  	print "Usage: set_property.py <hostname>"
+	sys.exit(1)
+
+  hostname = sys.argv[1]
+
   con = pbs.pbs_connect(pbs_server)
 
   attrop_l = pbs.new_attropl(1)
-  attrop_l[0].name  = 'properties'
+  attrop_l[0].name  = 'note'
   attrop_l[0].value = 'set_something_useful'
-  attrop_l[0].op    = pbs.INCR
+  attrop_l[0].op    = pbs.SET
 
   r =  pbs.pbs_manager(con, pbs.MGR_CMD_SET, pbs.MGR_OBJ_NODE, 
-                    "e2", attrop_l, 'NULL')
+                    hostname, attrop_l, 'NULL')
 
   if r > 0:
     print r, ";"
